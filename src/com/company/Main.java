381b4d7f881f0d223a1483b63b691a8500500010
Eka2020 <60359521+Eka2020@users.noreply.github.com>
2020-03-24 12:10:23 +0600

    LAST

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,24 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 
 public class Main {
     public static final String ANSI_BLUE = " \u001B[34m";
+    public static final String ANSI_GREEN = " \u001B[36m";
+    public static final String ANSI_Green = "\u001B[32m";
 
     public static void main(String[] args) {
         Semaphore sem = new Semaphore(4, true);
         CountDownLatch latch = new CountDownLatch(100);
+
+        System.out.println(ANSI_GREEN + "Текущая дата: " + new SimpleDateFormat("dd.MM.yyyy")
+                .format(Calendar.getInstance().getTime()));
+        System.out.println(ANSI_Green + "В автовокзале " + latch.getCount() + " пассажиров ожидают отправки в город Ош");
+        System.out.println(ANSI_Green + "Билеты можно приобрести в 4 кассах автовокзала");
+
 
         for (int i = 1; i <= 100; i++) {
             new Passenger(sem, i, latch).start();
@@ -19,5 +29,7 @@ public class Main {
             System.out.println(ANSI_BLUE + "Поехали!");
         } catch (InterruptedException e) {
         }
+
     }
+
 }
